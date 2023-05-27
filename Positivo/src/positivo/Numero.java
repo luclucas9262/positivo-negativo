a32c7c4f4a61c4bddae88783ca9a0f6b1bcc95da
luclucas9262 <lucaseleuterio95@gmail.com>
2023-05-27 15:03:09 -0300

    ajustes

--- Positivo/src/positivo/Numero.java
+++ Positivo/src/positivo/Numero.java
@@ -4,29 +4,27 @@ import java.util.Scanner;
 
 public class Numero {
 
-    private int a, b;
+    private Double a, b;
 
     Scanner entrada = new Scanner(System.in);
 
-    
-    
     public void SetN1() {
         System.out.println("Digite um número: ");
-        this.a = entrada.nextInt();
         
+        this.a = entrada.nextDouble();
+
     }
 
-    public int GetN1() {
+    public double GetN1() {
         return this.a;
     }
-    
-    
-    
-    public void SetNegativo(){
-        System.out.println("NEGATIVO: "+ a);
+
+    public void SetNegativo() {
+        System.out.println("NEGATIVO: " + a);
     }
-    public void SetPositivo(){
-        System.out.println("NAO NEGATIVO "+ a);
+
+    public void SetPositivo() {
+        System.out.println("NAO NEGATIVO " + a);
     }
 
 }
